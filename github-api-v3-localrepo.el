f4454c997212eb01bd9b487838873a10e46bd836
Robert Sanders <robert@zeevex.com>
2012-02-06 16:57:05 -0500

    add license

--- github-api-v3-localrepo.el
+++ github-api-v3-localrepo.el
@@ -1,3 +1,26 @@
+;;; githubv3.el --- implementation of GitHub API v3
+
+;; Copyright (c) 2012 Robert Sanders
+;; Copyright (c) 2010 Nathan Weizenbaum
+;; Licensed under the same terms as Emacs.
+
+;; Author: Robert Sanders
+;; URL: http://github.com/rsanders/github-api-v3.el
+;; Version: 0.1
+;; Created: 2012-02-05
+;; By: Nathan Weizenbaum
+;; Keywords: git, github, magit, api
+;; Package-Requires: ((magit "0.8") (json "1.2"))
+
+;; Borrowing *heavily* from the partial GitHub v2 API included in Magithub:
+
+;; Author: Nathan Weizenbaum
+;; URL: http://github.com/nex3/magithub
+;; Version: 0.1
+;; Created: 2010-06-06
+;; By: Nathan Weizenbaum
+
+
 ;;;
 ;;; NOT CONVERTED TO V3 YET
 ;;;
